163979d5446aa69c3e863bbca2fb3688df6a8dac
Sri Nidhi <nidhikuchana@gmail.com>
2026-09-01 18:26:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import streamlit as st
 
 
 st.set_page_config(
-    page_title="Farmer Decision Support Platform",
+    page_title="AgriSense",
     page_icon=None,
     layout="wide",
     initial_sidebar_state="collapsed",
